310e79211515e7e19cda8d4b2e43c0b2d12044b2
its-bright28 <its.not.bright28@gmail.com>
2026-08-13 18:05:02 +0100

    My first repository

---
+++ script.js
@@ -0,0 +1,104 @@
+function myMenuFunction () {
+    var menuBth =document.getElementById("myNavmenu");
+
+    if  (menuBth.className === "nav-menu") {
+        menuBth.className += " responsive";
+    } else {
+        menuBth.className = "nav-menu"
+    }
+
+}
+
+
+
+
+/*---------Dark mode-----------*/
+
+const body = document.querySelector("body"),
+    toggleswitch = document.getElementById("toggle-switch");
+
+toggleswitch.addEventListener("click", ()=> {
+    body.classList.toggle("dark");
+});
+
+// Typing Effect
+
+var typingEffect = new Typed(".typedText", {
+    strings: ["Designer", "coder", "Developer"],
+
+    loop: true,
+    typespeed: 100,
+    backspeed: 80,
+    backDelay: 2000,
+// this function did not work
+
+})
+
+// scroll animation
+
+const sr = scrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+sr.reveal(".featured-name", { delay:100 });
+sr.reveal(".text-info", { delay: 200});
+sr.reveal(".text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 320 });
+
+
+sr.reveal(".project-box",{ interval: 200});
+
+sr.reveal(".top-header", {});
+
+
+const srLeft = scrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+srLeft.reveal(".about-info", { delay: 100});
+srLeft.reveal(".contact-info", { delay: 100});
+
+const srRight = scrollReveal({
+    origin: "Left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+srRight.reveal(".skill", { delay: 100});
+srRight.reveal(".skill-box", { delay: 100});
+
+// active link
+
+const sections = document.querySelectorAll(".section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = (current.offsetTop = 50),
+      sectionId = current.getAttribute("id");
+    // i formatted selction
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+      // when i formatted selection i dont think it worked
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
